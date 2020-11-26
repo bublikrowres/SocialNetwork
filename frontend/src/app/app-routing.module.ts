05d4f28164b31b52d0edd136596303e35feb8350
Rares <bublikrowres@yahoo.com>
2020-11-26 20:23:42 +0200

    More configs

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ children: [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
